849d543988e17bdc2d45537576c773b8b6c435d7
Scheyk <kathrin.dostal@gmail.com>
2020-07-16 08:56:33 +0200

    ALL DEBUGgit add .git add .WUPIIIIIgit add .!

---
+++ car_rental/delete_car.php
@@ -0,0 +1,17 @@
+<?php
+	require_once 'actions/db_connecting.php';
+
+	if($_GET["id"]) {
+		$id = $_GET["id"];
+
+		$sql = "DELETE FROM cars WHERE id = $id";
+
+		if(mysqli_query($connect, $sql)) {
+			echo "sucess <br>
+			 	  <a href='index.php'>back home</a>";
+		} else {
+			echo "error4";
+		}
+
+	}
+?>
